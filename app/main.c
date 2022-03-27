f6960381f3cb246dfd9a84fc5e94aada8e2e8762
Cuudemi <99761650+Cuudemi@users.noreply.github.com>
2022-03-27 20:23:02 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -5,24 +5,6 @@
 
 int main ()
 {
-    // float x1 = 0;
-    // float x2 = 0;
-    // float x3 = 0;
-    //  float a, b, c;
-    // printf ("Введите коэффициенты уравнения\n");
-    // scanf ("%f %f %f", &a, &b, &c);
-    // int fl = SquareEquality(a, b, c, &x1, &x2, &x3);
-    //  if (fl == 2) {
-        // printf ("Корни: %g, %g\n", x1, x2);
-        // return 0;
-        // }
-    // if (fl == 1) {
-        // printf  ("Корень: %g \n", x3);
-        // return 0;
-        // }
-    // else {
-        // printf ("Решений нет\n");
-        // return 0;
-        // }
+
     return 0;
 }
